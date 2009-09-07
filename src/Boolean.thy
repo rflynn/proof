d4ce2117b6201b62956f887060a978cdddab60c0
pizza <pizza@ubuntu.(none)>
2009-09-07 11:46:26 -0400

    clean up Boolean format. progress on Vector

--- src/Boolean.thy
+++ src/Boolean.thy
@@ -5,8 +5,11 @@ begin
 
 types gate = "bool \<Rightarrow> bool \<Rightarrow> bool"
 
-definition xor :: gate
-where "xor x y \<equiv> (x \<or> y) \<and> (\<not> (x \<and> y))"
+value "True & False"
+value "True | False"
+
+definition xor :: gate where
+"xor x y \<equiv> (x \<or> y) \<and> (\<not> (x \<and> y))"
 
 theorem xor_neq [simp]: "(xor x y) = (x \<noteq> y)"
 apply(simp only: xor_def)
@@ -22,24 +25,24 @@ apply(simp only: xor_def)
 apply(auto)
 done
 
-definition nor :: gate
-where "nor x y \<equiv> \<not>(x \<or> y)"
+definition nor :: gate where
+"nor x y \<equiv> \<not>(x \<or> y)"
 
 theorem nor_self [simp]: "nor x (\<not>x) = False"
 apply(simp only: nor_def)
 apply(auto)
 done
 
-definition xnor :: gate
-where "xnor x y \<equiv> (x \<and> y) \<or> \<not>(x \<or> y)"
+definition xnor :: gate where
+"xnor x y \<equiv> (x \<and> y) \<or> \<not>(x \<or> y)"
 
 theorem xnor_eq [simp]: "xnor x y = (x = y)"
 apply(simp only: xnor_def)
 apply(auto)
 done
 
-definition nand :: gate
-where "nand x y \<equiv> (\<not>(x \<and> y))"
+definition nand :: gate where
+"nand x y \<equiv> (\<not>(x \<and> y))"
 
 theorem nand_xx [simp]: "nand x x \<equiv> \<not>x"
 apply(simp only: nand_def)
--- src/Vector.thy
+++ src/Vector.thy
@@ -1,29 +1,139 @@
 
 theory Vector 
-imports Main
+imports Main Boolean
 begin
 
-types ('a)vector = "('a)list"
-      bitv       = "(bool)list"
+(* TODO: convert to/from integer value *)
 
-value "[False]"
-value "[False] # [False] # []"
+types 'a vector = "'a list"
+      bitv      = "bool list"
 
-(* construct bitv of fixed size *)
-fun bitv_new2 :: "nat \<Rightarrow> bitv \<Rightarrow> bitv" where
-"bitv_new2 0 xs = xs" |
-"bitv_new2 (Suc n) [] = bitv_new2 n [False]" |
-"bitv_new2 (Suc n) xs = bitv_new2 n (False # xs)"
+definition new :: "nat \<Rightarrow> bitv" where
+"new n \<equiv> replicate n False"
 
-value "bitv_new2 0 []"
-value "bitv_new2 1 []"
-value "bitv_new2 2 []"
+(* we get equality "=" for free with list *)
 
-definition bitv_new :: "nat \<Rightarrow> bitv" where
-"bitv_new n = bitv_new2 n []"
+fun zipWith :: "('a \<Rightarrow> 'b \<Rightarrow> 'c) \<Rightarrow> 'a list \<Rightarrow> 'b list \<Rightarrow> 'c list" where
+"zipWith f [] _ = []" |
+"zipWith f _ [] = []" |
+"zipWith f (x#xs) (y#ys) = (f x y) # (zipWith f xs ys)"
 
-value "bitv_new 1"
+datatype eq = Eq | Lt | Gt
 
-fun xor
+(* NOTE: for mismatched sizes, we only account for the smallest size *)
+fun cmp :: "bitv \<Rightarrow> bitv \<Rightarrow> eq" where
+"cmp [] _ = Eq" |
+"cmp _ [] = Eq" |
+"cmp (False # xs) (True # ys) = Lt" |
+"cmp (True # xs) (False # ys) = Gt" |
+"cmp (x # xs) (y # ys) = cmp xs ys"
+
+(* NOTE: for mismatched sizes, we only account for the smallest size *)
+fun lt2 :: "bitv \<Rightarrow> bitv \<Rightarrow> bool" where
+"lt2 [] _ = False" |
+"lt2 _ [] = False" |
+"lt2 (False # xs) (True # ys) = True" |
+"lt2 (True # xs) (False # ys) = False" |
+"lt2 (x # xs) (y # ys) = lt2 xs ys"
+
+definition lt :: "bitv \<Rightarrow> bitv \<Rightarrow> bool" (infixr "<" 50) where
+"lt xs ys \<equiv> lt2 (rev xs) (rev ys)"
+
+value "[True] < [False]"
+value "[True] < [True]"
+value "[False,True] < [True,False]"
+value "[True,False] < [False,True]"
+value "[] < [False]"
+
+fun lteq2 :: "bitv \<Rightarrow> bitv \<Rightarrow> bool" where
+"lteq2 [] _ = True" |
+"lteq2 _ [] = True" |
+"lteq2 (False # xs) (True # ys) = True" |
+"lteq2 (True # xs) (False # ys) = False" |
+"lteq2 (x # xs) (y # ys) = lteq2 xs ys"
+
+definition lteq :: "bitv \<Rightarrow> bitv \<Rightarrow> bool" (infixr "\<le>" 50) where
+"lteq xs ys \<equiv> lteq2 (rev xs) (rev ys)"
+
+fun gt2 :: "bitv \<Rightarrow> bitv \<Rightarrow> bool" where
+"gt2 [] _ = False" |
+"gt2 _ [] = False" |
+"gt2 (False # xs) (True # ys) = False" |
+"gt2 (True # xs) (False # ys) = True" |
+"gt2 (x # xs) (y # ys) = gt2 xs ys"
+
+definition gt :: "bitv \<Rightarrow> bitv \<Rightarrow> bool" (infixr ">" 50) where
+"gt xs ys \<equiv> gt2 (rev xs) (rev ys)"
+
+value "[False] > [True]"
+value "[False,True] > [True,False]"
+value "[True] > [True]"
+
+definition and2 :: gate where "and2 x y \<equiv> x \<and> y"
+definition band :: "bitv \<Rightarrow> bitv \<Rightarrow> bitv" (infixr "&" 50) where
+"band xs ys \<equiv> zipWith and2 xs ys"
+
+value "True & False"
+value "[True] & [False]"
+
+(*
+theorem band_equiv: "hd(x & y) = (hd(x)) & (hd(y))"
+apply(simp only: band_def)
+apply(auto)
+*)
+
+(* wrap or so it can be used as a parameter to zipWith *)
+definition or :: gate where "or x y \<equiv> x \<or> y"
+
+definition bor :: "bitv \<Rightarrow> bitv \<Rightarrow> bitv" (infixr "|" 50) where
+"bor xs ys \<equiv> zipWith or xs ys"
+
+value "True | False"
+value "[True] | [False]"
+
+fun shr :: "bitv \<Rightarrow> nat \<Rightarrow> bitv" (infixr "\<guillemotright>" 50) where
+"shr [] _ = []" |
+"shr xs n = (if n > size xs
+             then (replicate (size xs) False)
+             else (replicate n False) @ (take ((size xs)-n) xs))"
+
+value "shr [True,True] 2"
+value "shr [True,True] 3"
+
+(* \<guillemotright>0 is identity *)
+theorem shr_0 [simp]: "xs \<guillemotright> 0 = xs"
+apply(induct_tac xs)
+apply(auto)
+done
+
+(* shifting by more than length equivalent to shifting by length *)
+theorem shr_toomany: "xs \<guillemotright> (length xs) = (xs \<guillemotright> ((length xs) + 1))"
+apply(induct_tac xs)
+apply(simp)
+apply(auto)
+done
+
+(* shifting by the full length is equivalent to setting value to all False *)
+theorem shr_max_val: "xs \<guillemotright> (length xs) = replicate (length xs) False"
+apply(induct_tac xs)
+apply(auto)
+done
+
+(* shifting preserves the size of a vector *)
+theorem shr_preserve_size: "length xs = length (xs \<guillemotright> n)"
+apply(induct_tac xs)
+apply(auto)
+done
+
+fun shl :: "bitv \<Rightarrow> nat \<Rightarrow> bitv" (infixr "\<guillemotleft>" 50) where
+"shl [] _ = []" |
+"shl xs n = (if n > size xs
+             then (replicate (size xs) False)
+             else (take ((size xs)-n) xs) @ (replicate n False))"
+
+value "[True,True] \<guillemotleft> 0"
+value "[True,True] \<guillemotleft> 1"
+value "[True,True] \<guillemotleft> 2"
+value "[True,True] \<guillemotleft> 3"
 
 end
